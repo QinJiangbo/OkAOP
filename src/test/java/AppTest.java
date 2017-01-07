89185904d36c269389d1337eb03c7ce8655df7d6
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-01-07 20:37:29 +0800

    adjust project structure

---
+++ src/test/java/AppTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+/**
+ * @date: 07/01/2017 8:36 PM
+ * @author: qinjiangbo@github.io
+ */
+public class AppTest {
+
+    @Test
+    public void test() {
+        System.out.println("Hello AOP!");
+    }
+}
