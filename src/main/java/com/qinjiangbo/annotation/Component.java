6b8ffa5eb9cbfb84e2c50a701f2eda5c7095e187
Qin Jiangbo <qinjiangbo@users.noreply.github.com>
2017-01-07 20:52:34 +0800

    add annotations @Finally, @Component, @Throwing

---
+++ src/main/java/com/qinjiangbo/annotation/Component.java
@@ -0,0 +1,13 @@
+package com.qinjiangbo.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * @date: 07/01/2017 8:50 PM
+ * @author: qinjiangbo@github.io
+ */
+@Target(ElementType.TYPE)
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Component {
+}
---
+++ src/main/java/com/qinjiangbo/annotation/Finally.java
@@ -0,0 +1,13 @@
+package com.qinjiangbo.annotation;
+
+import java.lang.annotation.*;
+
+/**
+ * @date: 07/01/2017 8:45 PM
+ * @author: qinjiangbo@github.io
+ */
+@Target(ElementType.METHOD)
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Finally {
+}
